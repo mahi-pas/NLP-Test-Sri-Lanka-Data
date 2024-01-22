91ed0314f129dada80be9bb25c2a82f7d91d60d6
EggyRepublic <Eggyrepublic@gmail.com>
2024-01-22 03:04:08 -0500

    read pdf with xpdf
    will be testing with other libraries to see if I can get a cleaner table conversion.

--- PDFoperations.py
+++ PDFoperations.py
@@ -1,15 +1,22 @@
 #Alan
 
-'''
-Arguments: filename of PDF
-Returns: formatted PDF
-'''
-def formatPDF(filename):
-    pass
+import subprocess as sp
+import pathlib
 
-'''
-Arguments: pdf file
-Returns: string of data from rtf
-'''
-def PDFtoRTF(pdf):
-    pass
+def PDFtoRTF(path, output_file):
+    #Generate a text rendering of a PDF file in the form of a list of lines.
+    args = ['pdftotext', '-layout', path, output_file]
+    cp = sp.run(
+      args, stdout=sp.PIPE, stderr=sp.DEVNULL,
+      check=True, text=True
+    )
+    return cp.stdout
+
+def runner():
+    input_file = 'Data/Dengue/dengue sri lanka.pdf'
+    output_name = input_file.split('/')[-1]
+    pathlib.Path('PDFAsText').mkdir(parents=True, exist_ok=True) 
+    print(PDFtoRTF(input_file, f'PDFAsText/{output_name}.txt'))
+
+if __name__ == '__main__':
+    runner()
