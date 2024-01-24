8f1685d2899d54589e5eccd1c873766fd86aafab
JS <67136819+skyjune0824@users.noreply.github.com>
2024-01-24 15:44:18 -0500

    .

--- LLaMaInterface.py
+++ LLaMaInterface.py
@@ -17,18 +17,18 @@ def separateCellHeaders(row):
     RDHS Dengue Fever Dysentery Encephaliti Enteric Fever Food Poi- Leptospirosis Typhus Viral Hep- Human Chickenpox Meningitis Leishmania- WRCD
     '''
     pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as Assistant."
-    prompt = "Seperate these into different titles for columns using commas: " + row
+    prompt = "Seperate these into different titles for columns using commas: \n" + row
     
     LLM = Llama(model_path="./llama-2-7b-chat.Q5_K_M.gguf", n_ctx=2048)
     # WARNING! ggml is no longer compatable with llama_cpp, so use gguf models
 
-    output = LLM(f"{prompt}", max_tokens=0)
+    output = LLM(f"{row}", max_tokens=0)
 
     return output["choices"][0]["text"]
 
 now = time.time()
 
-test_out = separateCellHeaders("RDHS Dengue Fever Dysentery Encephaliti Enteric Fever Food Poi- Leptospirosis Typhus Viral Hep- Human Chickenpox Meningitis Leishmania- WRCD")
+test_out = separateCellHeaders("What is the coordinates of Colombo, Sri Lanka (as specific as possible)?")
 
 print(test_out)
 
