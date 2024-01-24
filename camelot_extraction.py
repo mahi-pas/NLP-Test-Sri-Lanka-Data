445712ba6678cf3564e28c5cd6ffcfa2d657cce4
EggyRepublic <Eggyrepublic@gmail.com>
2024-01-24 17:23:50 -0500

    rename

--- camelot_extraction.py
+++
@@ -1,8 +0,0 @@
-import camelot
-import pandas
-
-tables = camelot.read_pdf('Data/Dengue/sri lanka weekly.pdf')
-print(f'tables: \n{tables}\n\n')
-
-tables.export('result.csv', f='csv', compress=False) # json, excel, html, markdown, sqlite
-print(f'report: {tables[0].parsing_report}')
