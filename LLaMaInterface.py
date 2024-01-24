f5dcb5239642d0c4caf38fecd19da716e4fd0e25
JS <67136819+skyjune0824@users.noreply.github.com>
2024-01-24 15:13:25 -0500

    Local inputted

--- LLaMaInterface.py
+++ LLaMaInterface.py
@@ -1,12 +1,8 @@
 # Jun
-# I'll look for another way to use LLM instead of this, but just for testing.
+# Local version of LLaMaInterface
 
-import os
-import replicate
-# Using replicate to authrize, and then run the model (Online, not entirely free, but will not cost for short run)
-
-os.environ["REPLICATE_API_TOKEN"] = "r8_"
-# "r8_8fIrIK5FiWIN4c2tlL6KQSgp5NaeD6k3ZGyYB" <-- don't use it for now
+from llama_cpp import Llama
+import time
 
 '''
 Arguments: String with header file
@@ -20,24 +16,21 @@ def separateCellHeaders(row):
 
     RDHS Dengue Fever Dysentery Encephaliti Enteric Fever Food Poi- Leptospirosis Typhus Viral Hep- Human Chickenpox Meningitis Leishmania- WRCD
     '''
-    pre_prompt = "You are a helpful assistant. Always answer as concise as possible. You only response once as 'Assistant'."
-    prompt = "Seperate these into different titles for columns: " + row
+    pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as Assistant."
+    prompt = "Seperate these into different titles for columns using commas: " + row
     
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', #LMM address
-                           input={
-                               "prompt": f"{pre_prompt} {prompt}",
-                               "temperature": 0.75,
-                               "top_p":0.9,
-                               "max_length":128,
-                               "max_new_tokens": 500,
-                               "min_new_tokens": -1,
-                               })
-    
-    response = ""
+    LLM = Llama(model_path="./llama-2-7b-chat.Q5_K_M.gguf", n_ctx=2048)
+    # WARNING! ggml is no longer compatable with llama_cpp, so use gguf models
 
-    for tokens in output:
-        response += tokens
+    output = LLM(f"{prompt}", max_tokens=0)
 
-    return response
+    return output["choices"][0]["text"]
 
-separateCellHeaders("RDHS Dengue Fever Dysentery Encephaliti Enteric Fever Food Poi- Leptospirosis Typhus Viral Hep- Human Chickenpox Meningitis Leishmania- WRCD")
+now = time.time()
+
+test_out = separateCellHeaders("RDHS Dengue Fever Dysentery Encephaliti Enteric Fever Food Poi- Leptospirosis Typhus Viral Hep- Human Chickenpox Meningitis Leishmania- WRCD")
+
+print(test_out)
+
+finished = time.time()
+print(finished - now, "seconds elapsed.")
